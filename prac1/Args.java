7d9557fdccfeea1bfc2232ae2d0f3b795bfa57d5
Otterion <92106428+Otterion@users.noreply.github.com>
2022-12-23 00:29:12 +0300

    Add files via upload

---
+++ prac1/Args.java
@@ -0,0 +1,11 @@
+package prac1;
+
+public class Args {
+    public static void main (String args[])
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            System.out.println(args[i]);
+        }
+    }
+}
